f4af4b9fd9ecdb5fd9dee0ed22f45f53bf6e53c4
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2022-09-24 13:56:24 +0900

    feat: 🎸 finish sample-prototype

---
+++ scripts/dialog.js
@@ -0,0 +1,79 @@
+import dialogPolyfill from 'dialog-polyfill';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
+export default function createDialogHandler({
+ element, container, topBar, bottomBar,
+}) {
+  dialogPolyfill.registerDialog(element);
+
+  const windowScrollPosition = { top: 0, left: 0 };
+  let ticking = false;
+
+  const handleScrollOnDialog = () => {
+    if (!ticking) {
+      requestAnimationFrame(() => {
+        ticking = false;
+
+        topBar.classList[element.scrollTop > 0 ? 'add' : 'remove']('is-sticky');
+
+        // iOSにて、最初からstickyにしておくと、DialogのbottomBarの表示が遅れるので、ファーストビューからスクロールされた時だけstickyで表示する
+        bottomBar.classList.add('is-sticky');
+      });
+
+      ticking = true;
+    }
+  };
+
+  const openDialog = () => {
+    windowScrollPosition.top = window.scrollY;
+    windowScrollPosition.left = window.scrollX;
+
+    // Dialogを開く
+    element.showModal();
+
+    // Dialogを開いたら、スクロールロック
+    disableBodyScroll(container, {
+      // iOSにて、Dialogもscroll lockされるので対処
+      // 参考: https://github.com/willmcpo/body-scroll-lock#allowtouchmove
+      allowTouchMove: true,
+    });
+  };
+
+  const closeDialog = () => {
+    // Dialogのスクロールを上に戻して、Dialogが開いた際の位置を初期化する
+    element.scrollTo(0, 0);
+
+    // Dialog外のスクロールロック解除
+    enableBodyScroll(container);
+
+    element.close();
+
+    // iOSにて、最初からstickyにしておくと、DialogのbottomBarの表示が遅れるので、表示している時だけstickyで表示する
+    bottomBar.classList.remove('is-sticky');
+
+    // MEMO: iOS(WebKit)にて、画面全体のscrollが一緒に初期化されるバグがあるので、保存しておいた値を代入
+    window.scrollTo(windowScrollPosition);
+  };
+
+  const handleKeydownESC = (e) => {
+    if (!e.repeat && e.keyCode === 27 && element.open) {
+      closeDialog();
+    }
+  };
+
+  // 背景クリックの監視
+  element.addEventListener('click', (e) => {
+    // はみ出たdialog背景をクリックした際に、Dialogを閉じる
+    if (e.target === element) {
+      closeDialog();
+    }
+  });
+
+  element.addEventListener('scroll', handleScrollOnDialog);
+  document.addEventListener('keydown', handleKeydownESC);
+
+  return {
+    openDialog,
+    closeDialog,
+  };
+}
---
+++ scripts/sample-dialog.js
@@ -0,0 +1,91 @@
+import createDialogHandler from './dialog.js';
+
+
+const sampleDialogElement = document.getElementById('sample-dialog-element');
+
+const initSampleDialog = () => {
+  //
+  // DOM取得
+  //
+
+  // dialogのelement取得
+  const sampleDialogContainer = document.getElementById(
+    'sample-dialog-container',
+  );
+  const sampleDialogTopBar = document.getElementById('sample-dialog-top-bar');
+  const sampleDialogBottomBar = document.getElementById('sample-dialog-bottom-bar');
+  const sampleDialogOpenButton = document.getElementById(
+    'sample-dialog-open-button',
+  );
+  const sampleDialogResetButton = document.getElementById(
+    'sample-dialog-reset-button',
+  );
+  const sampleDialogSubmitButton = document.getElementById(
+    'sample-dialog-submit-button',
+  );
+
+  //
+  // Dialog内の処理
+  //
+
+
+
+
+  //
+  // Dialog外のViewの処理
+  //
+
+
+  //
+  // handler
+  //
+
+
+  //
+  // EventListener
+  //
+
+  //
+  // Init処理
+  //
+
+
+  //
+  // DialogのInit処理
+  //
+
+  // Create Dialog
+  const { openDialog, closeDialog } = createDialogHandler({
+    element: sampleDialogElement,
+    container: sampleDialogContainer,
+    topBar: sampleDialogTopBar,
+    bottomBar: sampleDialogBottomBar,
+  });
+
+  const openSampleDialog = () => {
+    // 開く前に処理したいことあれば記述
+
+    openDialog();
+  };
+
+  const resetSampleDialog = () => {
+    // 閉じる前に処理したいことあれば記述
+
+    closeDialog();
+  };
+
+  const submitSampleDialog = () => {
+    // 閉じる前に処理したいことあれば記述
+
+    closeDialog();
+  };
+
+  // Dialogのclickイベント登録
+  sampleDialogOpenButton.addEventListener('click', openSampleDialog);
+  sampleDialogResetButton.addEventListener('click', resetSampleDialog);
+  sampleDialogSubmitButton.addEventListener('click', submitSampleDialog);
+};
+
+if (sampleDialogElement) {
+  initSampleDialog();
+}
